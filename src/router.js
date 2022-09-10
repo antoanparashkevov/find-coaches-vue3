cbc3bf6412b25591c1a04aad1db7f95cbd3e2c70
“antoanparashkevov” <tonkata1505@gmail.com>
2022-09-10 09:45:14 +0300

    Added the necessary components + registering into routes

--- src/router.js
+++ src/router.js
@@ -1,16 +1,25 @@
 import { createRouter,createWebHistory } from 'vue-router';
 
+//import components...
+import CoachDetails from '@/pages/coaches/CoachDetails';
+import CoachesList from '@/pages/coaches/CoachesList';
+import ContactCoach from '@/pages/coaches/ContactCoach';
+import CoachRegistration from '@/pages/requests/CoachRegistration';
+import RequestsReceived from '@/pages/requests/RequestsReceived'
+import notFound from '@/pages/notFound';
+
 const router = createRouter({
   history:createWebHistory(),
   routes:[
     {path:'/',redirect:'/coaches'},
-    {path:'/coaches',component:null},
-    {path:'/coaches/:id',component:null,
+    {path:'/coaches',component:CoachesList},
+    {path:'/coaches/:id',component:CoachDetails,
     children:[
-      { path: 'contact', component:null }
+      { path: 'contact', component:ContactCoach }
     ]},
-    {path:'/requests',component:null},
-    {path:'/register',component:null}
+    {path:'/requests',component:RequestsReceived},
+    {path:'/register',component:CoachRegistration},
+    {path:'notFound(.*)',component:notFound}
   ]
 })
 
