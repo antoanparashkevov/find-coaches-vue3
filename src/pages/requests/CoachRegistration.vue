efb04e78b1095d39937f3eff0bb22fb3704b591f
“antoanparashkevov” <tonkata1505@gmail.com>
2022-09-11 12:47:13 +0300

    Register as a coach form structure + styles

--- src/pages/requests/CoachRegistration.vue
+++ src/pages/requests/CoachRegistration.vue
@@ -1,12 +1,19 @@
 <template>
   <section>
-   REGISTRATION
+   <base-card>
+     <h2>Register as a coach now!</h2>
+     <coach-form></coach-form>
+   </base-card>
   </section>
 </template>
 
 <script>
+import CoachForm from '../../coaches/CoachForm';
 export default {
-  name: 'CoachRegistration'
+  name: 'CoachRegistration',
+  components:{
+    CoachForm
+  }
 };
 </script>
 
