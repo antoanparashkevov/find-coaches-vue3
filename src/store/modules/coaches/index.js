f0b49a2815ec52aa66808dd9e102c2e4ecd51361
“antoanparashkevov” <tonkata1505@gmail.com>
2022-09-19 20:33:11 +0300

    removed hard coded coaches

--- src/store/modules/coaches/index.js
+++ src/store/modules/coaches/index.js
@@ -6,32 +6,7 @@ export default {
   namespaced:true,
   state(){
     return {
-      coaches:[
-        {
-          id: 'c1',
-          firstName:'Antoan',
-          lastName:'Parashkevov',
-          areas:['frontend','career','backend'],
-          description:'Some description 1',
-          hourlyRate:30
-        },
-        {
-          id: 'c2',
-          firstName:'Nadia',
-          lastName:'Dolashka',
-          areas:['career'],
-          description:'Some description 2',
-          hourlyRate:40
-        },
-        {
-          id: 'c3',
-          firstName:'Svetlio',
-          lastName:'Parashkevov',
-          areas:['frontend','backend'],
-          description:'Some description 3',
-          hourlyRate:50
-        },
-      ]
+      coaches:[]
     }
   },
   mutations,
