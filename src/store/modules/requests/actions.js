79f7707109ff097084f2571eb33a405b33e0dab9
“antoanparashkevov” <tonkata1505@gmail.com>
2022-09-22 14:36:46 +0300

    added fetch request for post the requests

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,23 @@
 export default {
-  addRequest(context, payload){
+  async addRequest(context, payload){
     const newRequest = {
-      id: new Date().toISOString(),
       coachId: payload.coachId,
       email:payload.email,
       message:payload.message
     }
+    
+    const response = await fetch('https://parseapi.back4app.com/classes/requests', {
+      method: 'POST',
+      headers: {
+      'X-Parse-Application-Id': '63u3mbmETZ0kesrnNGo1XTas8yKa8HdbqYdbj2sf',
+      'X-Parse-REST-API-Key': 'oU9VIRLkWZlFbxozm4ZcY13n1tCOnQY2usqgaPSi',
+      'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newRequest)
+    })
+    const responseData = response.json();
+    
+    
     context.commit('addRequest',newRequest)
   }
 
