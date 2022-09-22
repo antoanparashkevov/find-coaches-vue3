0bca36237a8cbfcfc0a1081fdc86a9eeed898731
“antoanparashkevov” <tonkata1505@gmail.com>
2022-09-22 15:45:30 +0300

    Mutation method for getting the requests from the backend

--- src/store/modules/requests/mutations.js
+++ src/store/modules/requests/mutations.js
@@ -1,5 +1,9 @@
 export default {
   addRequest(state, payload) {
     state.requests.push(payload)
+  },
+  loadRequest(state,payload){
+    //We assume that the payload is all fetched requests from the backend
+    state.requests = payload
   }
 }
